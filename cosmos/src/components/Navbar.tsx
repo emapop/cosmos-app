794ff7cd17067fc4e11cf734b345ae4ff4570480
emapop <41983053+emapop@users.noreply.github.com>
2023-08-26 19:25:48 +0300

    changed attributes from navbar, colors, sizez

--- cosmos/src/components/Navbar.tsx
+++ cosmos/src/components/Navbar.tsx
@@ -15,23 +15,23 @@ const Navbar:React.FunctionComponent = () => {
                     <img className ='nav-cosmos-logo'src={CosmosLogo} alt='cosmos logo' />
                 </div>
                 <div className='nav-content-position nav-content-margin'>
-                <img className='nav-home-logo nav-space' src={HomeLogo} alt='home logo'/>
-                <p>Home</p>
+                <img className='nav-logo-menu nav-space' src={HomeLogo} alt='home logo'/>
+                <a  href=''>Home</a>
                 </div>
                 <div className='nav-content-position nav-content-margin'>
-                <img className='nav-home-logo nav-space' src={MoonLogo} alt='home logo'/>
-                <p>Destintion</p>
+                <img className='nav-logo-menu nav-space' src={MoonLogo} alt='home logo'/>
+                <a  href=''>Destintion</a>
                 </div>
                 <div className='nav-content-position nav-content-margin'>
-                <img className='nav-home-logo nav-space' src={SpaceshipLogo} alt='home logo'/>
-                <p>Spaceship</p>
+                <img className='nav-logo-menu nav-space' src={SpaceshipLogo} alt='home logo'/>
+                <a href=''>Spaceship</a>
                 </div>
                
             </div>
 
             <div className='nav-content-position'>
-                <img className='nav-bascket nav-space' src={BascketLogo} alt='basket logo'/>
-                <p>0</p>
+                <img className='nav-logo-menu nav-space' src={BascketLogo} alt='basket logo'/>
+                <a href=''>0</a>
             </div>
         </div>
     )
